17cfdee8796359dd8633eaf552792ff6cb6a7246
wgh <392235259@qq.com>
2019-07-08 22:22:44 +0800

    some comment

--- src/engine.cpp
+++ src/engine.cpp
@@ -9,10 +9,10 @@
 #include "comp/cfg_compute_asyn.h"
 
 
-static const char *const FINAL_FILE = "/home/zqzuo/Desktop/inlined/final";
-static const char *const STMT_INFO_FILE = "/home/zqzuo/Desktop/inlined/id_stmt_info.txt";
-static const char *const SINGLETON_FILE = "/home/zqzuo/Desktop/inlined/var_singleton_info.txt";
-static const char *const POINTTO_FILE = "/home/zqzuo/Desktop/inlined/rules_pointsto.txt";
+static const char *const FINAL_FILE = "/home/w/CLionProjects/backend-nju/lib/graphFile/final";      // todo 修改这里的文件路径
+static const char *const STMT_INFO_FILE = "/home/w/CLionProjects/backend-nju/lib/graphFile/id_stmt_info.txt";
+static const char *const SINGLETON_FILE = "/home/w/CLionProjects/backend-nju/lib/graphFile/var_singleton_info.txt";
+static const char *const POINTTO_FILE = "/home/w/CLionProjects/backend-nju/lib/graphFile/index_var_info.txt";           //todo 是不是有什么问题呢
 using namespace std;
 
 
